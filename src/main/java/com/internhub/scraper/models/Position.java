d81dabd77b813c4207aad821d5da21d7b9386646
David Hacker <dmhacker@protonmail.com>
2019-05-08 17:26:22 -0700

    Add many-to-one relationship for position, add getters/setters for position

--- src/main/java/com/internhub/scraper/models/Position.java
+++ src/main/java/com/internhub/scraper/models/Position.java
@@ -11,8 +11,33 @@ public class Position {
     private int id;
     @Column(name = "link")
     private String link;
-
+    @ManyToOne
+    @JoinColumn(name = "companyId")
     private Company company;
 
     public Position() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
