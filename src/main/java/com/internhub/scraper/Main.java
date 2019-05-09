5dc1b1460b66b69adb4bc6a73c5d2e53ed951b4d
David Hacker <dmhacker@protonmail.com>
2019-05-08 18:24:50 -0700

    Clean up Main to print website in addition to company name

--- src/main/java/com/internhub/scraper/Main.java
+++ src/main/java/com/internhub/scraper/Main.java
@@ -3,13 +3,11 @@ package com.internhub.scraper;
 import com.internhub.scraper.models.Company;
 import com.internhub.scraper.scrapers.RedditCompanyScraper;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         RedditCompanyScraper scraper = new RedditCompanyScraper();
         for (Company company : scraper.fetch()) {
-            System.out.println(company.getName());
+            System.out.println(company.getName() + " @ " + company.getWebsite());
         }
     }
 }
