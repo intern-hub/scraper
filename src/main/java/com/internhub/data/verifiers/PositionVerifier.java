d052c1f372ee14744d92fa6c86bd0e21a9c5b9d0
skrishnan99 <skrishnan99@icloud.com>
2019-05-15 21:49:47 -0700

    added initial heurisitics for application verifier and title extraction

--- src/main/java/com/internhub/data/verifiers/PositionVerifier.java
+++ src/main/java/com/internhub/data/verifiers/PositionVerifier.java
@@ -3,14 +3,53 @@ package com.internhub.data.verifiers;
 import com.internhub.data.models.Season;
 import org.jsoup.select.Elements;
 
+import java.util.*;
+
 public class PositionVerifier {
 
     public boolean isPositionValid(String applicationLink, Elements applicationPage) {
-        return false;
+
+        // TODO: Differentiate between general applications and internship applications - (eg - appearance of the word internship)
+        // TODO: Try to find more effective heuristics to add
+
+        int score = 0;
+        int threshold = 1;
+        
+        List<String> keyWords= Arrays.asList("apply", "start", "application", "submit");
+
+        Elements links = applicationPage.select("a[href]");
+        List<String> linkText = links.eachText();
+        
+        for(String keyStr: keyWords) {
+            for(String str: linkText) {
+                if(str.trim().toLowerCase().contains(keyStr)) {
+                    score += 1;
+                }                    
+            }
+        }
+
+        if (score >= threshold) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public String getPositionTitle(String applicationLink, Elements applicationPage) {
-        return null;
+
+        // TODO add more heuristics
+
+        if (this.isPositionValid(applicationLink, applicationPage)) {
+            List<String> title_list = applicationPage.select("h1").eachText();
+            if (title_list.size() > 0) {
+                return title_list.get(0);
+            }
+            else {
+                return null;
+            }
+            
+        }
     }
 
     public Season getPositionSeason(String applicationLink, Elements applicationPage) { return Season.SUMMER; }
